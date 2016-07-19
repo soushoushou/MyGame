5212eb0834bc0350eddd7f3fb7acc8a3e8056110
soushoushou <1186766429@qq.com>
2016-07-19 12:50:42 +0800

    漏传

---
+++ Classes/AppDelegate.cpp
@@ -0,0 +1,63 @@
+#include "GameScene.h"
+#include "AppDelegate.h"
+
+USING_NS_CC;
+
+static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
+static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
+static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
+static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
+
+AppDelegate::AppDelegate() {
+
+}
+
+AppDelegate::~AppDelegate()
+{
+}
+
+
+bool AppDelegate::applicationDidFinishLaunching() {
+	// initialize director
+	auto director = Director::getInstance();
+	auto glview = director->getOpenGLView();
+	if (!glview) {
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
+		glview = GLViewImpl::createWithRect("GitTest", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+#else
+		glview = GLViewImpl::create("GitTest");
+#endif
+		director->setOpenGLView(glview);
+	}
+	//改变分辨率
+	glview->setDesignResolutionSize(800, 600, ResolutionPolicy::NO_BORDER);
+	// turn on display FPS
+	director->setDisplayStats(true);
+
+	// set FPS. the default value is 1.0/60 if you don't call this
+	director->setAnimationInterval(1.0 / 60);
+
+	// create a scene. it's an autorelease object
+	auto scene = GameScene::scene();
+
+	// run
+	director->runWithScene(scene);
+
+	return true;
+}
+
+// This function will be called when the app is inactive. When comes a phone call,it's be invoked too
+void AppDelegate::applicationDidEnterBackground() {
+	Director::getInstance()->stopAnimation();
+
+	// if you use SimpleAudioEngine, it must be pause
+	// SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
+}
+
+// this function will be called when the app is active again
+void AppDelegate::applicationWillEnterForeground() {
+	Director::getInstance()->startAnimation();
+
+	// if you use SimpleAudioEngine, it must resume here
+	// SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
+}
