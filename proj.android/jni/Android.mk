3a4ec516cd81dec72c220bb192480c3958fd3a80
GitHubMarui <marui_156@163.com>
2016-07-24 19:51:00 +0800

    安卓生成配置文件，添加cpp文件时在jni/Anroid.mk文件中添加cpp文件路径

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -1,4 +1,7 @@
 LOCAL_PATH := $(call my-dir)
+NDK_TOOLCHAIN_VERSION=4.8
+
+APP_CPPFLAGS := -frtti -std=c++11
 
 include $(CLEAR_VARS)
 
@@ -7,13 +10,30 @@ $(call import-add-path,$(LOCAL_PATH)/../../cocos2d/external)
 $(call import-add-path,$(LOCAL_PATH)/../../cocos2d/cocos)
 $(call import-add-path,$(LOCAL_PATH)/../../cocos2d/cocos/audio/include)
 
+
 LOCAL_MODULE := MyGame_shared
 
 LOCAL_MODULE_FILENAME := libMyGame
 
+//添加新增加的cpp路径
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+                   ../../Classes/HelloWorldScene.cpp \
+                   ../../Classes/GameScene.cpp \
+                   ../../Classes/Global.cpp \
+				   ../../Classes/MainScene.cpp \
+				   ../../Classes/toolFunctions.cpp \
+                   ../../Classes/Player.cpp \
+                   ../../Classes/Poker.cpp \
+                   ../../Classes/Tool.cpp  \
+				   ../../Classes/User.cpp  \
+				   ../../Classes/ShadeLayer.cpp \
+				   ../../Classes/GamePlayScene.cpp \
+				   ../../Classes/LoginScene.cpp \
+				   ../../Classes/PopupLayer.cpp \
+				   ../../Classes/PopupScene.cpp \
+				   ../../Classes/TimeLayer.cpp
+
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
