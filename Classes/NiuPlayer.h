f79dd918b64d488b56720629e645e07a1527d758
GitHubMarui <marui_156@163.com>
2016-07-30 10:01:55 +0800

    漏传头文件

---
+++ Classes/NiuPlayer.h
@@ -0,0 +1,45 @@
+//
+//  NiuPlayer.hpp
+//  BullPoker
+//
+//  Created by 陈冬梅 on 16/7/25.
+//
+//
+
+#ifndef NiuPlayer_h
+#define NiuPlayer_h
+
+#include <stdio.h>
+#include "cocos2d.h"
+#include "GamePlayScene.h"
+
+USING_NS_CC;
+
+/** 玩家类型 */
+enum PlayerType
+{
+	/** 玩家自己 */
+	PlayerType_Me,
+	/** 右边玩家 */
+	PlayerType_Right,
+	/** 上部右边玩家 */
+	PlayerType_TopRight,
+	/** 上部左边玩家 */
+	PlayerType_TopLeft,
+	/** 左边玩家 */
+	PlayerType_Left
+};
+
+class NiuPlayer : public Object
+{
+public:
+	NiuPlayer();
+	~NiuPlayer();
+	void updatePkWeiZhi();
+
+private:
+	CC_SYNTHESIZE(__Array*, m_arrPk, ArrPk);
+	CC_SYNTHESIZE(Point, m_point, Point);
+	CC_SYNTHESIZE(PlayerType, m_iPlayerClass, PlayerClass);
+};
+#endif /* NiuPlayer_h */
