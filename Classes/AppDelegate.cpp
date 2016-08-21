7c75980954ed5d7eece076add907a2d095d3de7c
chendongmei2015 <chen_1404@foxmail.com>
2016-08-21 21:17:13 +0800

    帧率显示取消

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -60,7 +60,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
 	glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::EXACT_FIT);
 
 	// turn on display FPS
-	director->setDisplayStats(true);
+	director->setDisplayStats(false);
 
 	// set FPS. the default value is 1.0/60 if you don't call this
 	director->setAnimationInterval(1.0 / 60);
